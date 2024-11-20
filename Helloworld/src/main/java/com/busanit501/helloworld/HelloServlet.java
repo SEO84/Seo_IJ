ad7fa51b26e33ab8b9dc368f98538efb2e8593d8
이상용 <lsy3709@naver.com>
2024-11-20 14:31:24 +0900

    5교시 , 클라이언트 서버 , get, post 형식 폼방식으로 확인1

--- Helloworld/src/main/java/com/busanit501/helloworld/HelloServlet.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/HelloServlet.java
@@ -4,14 +4,22 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
+// 서블릿 파일
+// 자바 파일. -> 컴파일 class -> 서버가 로드 메모리 올려서 사용함.
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
+    // 라이프 사이클, 생명주기.
+    // 객체의 생명주기, 나중에, 프론트 앞단에서, 컴포넌트의 생명주기.
+    // 객체 생성 후, 소멸 하는 과정.
+
+    // 서버가 시작시, 최초로 한번 실행.
     public void init() {
         message = "Hello World!";
     }
 
+    //  get 호출시 마다, 메서드가 호출.
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
@@ -23,6 +31,7 @@ public class HelloServlet extends HttpServlet {
         out.println("</body></html>");
     }
 
+    // 서버가 종료시, 해당 객체를 반납하는 역할.
     public void destroy() {
     }
 }
