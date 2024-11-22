fb4012da032160446bdff27ee456ea0a789a22f5
이상용 <lsy3709@naver.com>
2024-11-22 10:27:30 +0900

    2교시, JDBC , 단위 테스트 기본 확인

---
+++ Helloworld/src/test/java/com/busanit501/helloworld/ConnectTest.java
@@ -0,0 +1,13 @@
+package com.busanit501.helloworld;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectTest {
+    @Test
+    public void test() {
+        int v1 = 100;
+        int v2 = 100;
+        Assertions.assertEquals(v1, v2);
+    }
+}
