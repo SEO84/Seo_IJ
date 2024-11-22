cc4625f76c70a7cff511194295a77e00bf405f24
이상용 <lsy3709@naver.com>
2024-11-22 12:13:22 +0900

    4교시, JDBC , Hikari CP, 라이브러리 이용해서, 디비 접근시 자원 이용을 효율적으로 하자. 오타수정

--- Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/ConnectionTest.java
+++ Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/ConnectionTest.java
@@ -14,7 +14,7 @@ public class ConnectionTest {
         hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
         hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
         hikariConfig.setUsername("webuser");
-        hikariConfig.setPassword("webdb");
+        hikariConfig.setPassword("webuser");
         // 캐쉬 기능이용해서, 메모리상에 임시 저장에서, 꺼내서 사용하겠다.
         // 얼마나 저장 할거냐? 250 길이로, 최대, 2048 까지 기억하겠다.
 
