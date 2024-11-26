2ed77aaadd045d75e274ee334feaba242bf671b8
이상용 <lsy3709@naver.com>
2024-11-26 15:22:10 +0900

    6교시, 서비스, 컨트롤러 연결, 하나 조회 서비스 작업, 단위테스트

--- Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/TodoServiceTest.java
+++ Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/TodoServiceTest.java
@@ -4,6 +4,7 @@ import com.busanit501.helloworld.jdbcex.dto.TodoDTO;
 import com.busanit501.helloworld.jdbcex.service.TodoService;
 
 import lombok.extern.log4j.Log4j2;
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,7 @@ public class TodoServiceTest {
         todoService = TodoService.INSTANCE;
     }
 
+    // 등록
     @Test
     public void testInsert() throws SQLException {
         // 더미 데이터, 화면에서 전달 받은 TodoDTO
@@ -30,12 +32,20 @@ public class TodoServiceTest {
         todoService.register(todoDTO);
     }
 
+    // 전체 조회
     @Test
     public void testSelectAll() throws SQLException {
          List<TodoDTO> dtoList = todoService.listAll();
         for (TodoDTO todoDto:dtoList) {
             log.info(todoDto);
         }
+    }
+
+    // 하나조회, 상세보기.
+    @Test
+    public void testSelectOne() throws SQLException {
+        val todoDTO = todoService.get(14L);
+        log.info("하나 조회. todoDTO " + todoDTO);
     }
 
 }
