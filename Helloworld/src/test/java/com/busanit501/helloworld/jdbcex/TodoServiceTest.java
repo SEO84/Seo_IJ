a6e6da30c761b1eb1f3651d054675c3f9375cc6c
이상용 <lsy3709@naver.com>
2024-11-27 10:45:59 +0900

    2교시,jdbcex , 수정 로직 처리, 서비스 기능 확인, 단위 테스트

--- Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/TodoServiceTest.java
+++ Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/TodoServiceTest.java
@@ -48,4 +48,20 @@ public class TodoServiceTest {
         log.info("하나 조회. todoDTO " + todoDTO);
     }
 
+    // 하나수정,
+    @Test
+    public void testUpdateOne() throws SQLException {
+        //
+        TodoDTO todoDTO = TodoDTO.builder()
+                .tno(14L)
+                .title("수정된 내용입니다.")
+                .dueDate(LocalDate.now())
+                .finished(false)
+                .build();
+
+        todoService.update(todoDTO);
+
+
+    }
+
 }
