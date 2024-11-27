f6def0a36d9fc675b142aee5cc5cd30cc82afb86
이상용 <lsy3709@naver.com>
2024-11-27 15:10:25 +0900

    6교시, 세션 정보, 로그인 체크 , 기본 로그인 페이지 화면 구성

---
+++ Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.busanit501.helloworld.jdbcex.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+@WebServlet(name = "LoginController" , urlPatterns = "/login")
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("LoginController doGet ");
+        request.getRequestDispatcher("/WEB-INFO/login.jsp")
+                .forward(request,response);
+    }
+}
